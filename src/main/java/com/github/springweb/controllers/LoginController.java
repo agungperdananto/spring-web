e6800bbced22e9234d43070273ddb6c7766f5dbb
Agung  Perdananto <agung.perdananto@sirclo.com>
2023-12-28 07:37:07 +0700

    add validation

--- src/main/java/com/github/springweb/controllers/LoginController.java
+++ src/main/java/com/github/springweb/controllers/LoginController.java
@@ -2,11 +2,14 @@ package com.github.springweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.github.springweb.models.Login;
+
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -22,9 +25,11 @@ public class LoginController {
     }
 
     @PostMapping("")
-    public String processLogin(Login login, Model model){
-
+    public String processLogin(@Valid Login login, BindingResult bindingResult, Model model){
         model.addAttribute("loginModel", login);
+        if (bindingResult.hasErrors()){
+            return "loginpage";
+        }
         return "loginresults";
     
     }
--- src/main/java/com/github/springweb/models/Login.java
+++ src/main/java/com/github/springweb/models/Login.java
@@ -1,5 +1,7 @@
 package com.github.springweb.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Login {
 
+    @NotBlank(message = "username can't be null")
+    @Size(min=3, max=20, message = "username length cannot more than 20 and less than 3 characters")
     private String username;
 
+    @NotBlank(message = "password can't be null")
+    @Size(min=6, message = "username length cannot less than 10 characters")
     private String password;
 }
